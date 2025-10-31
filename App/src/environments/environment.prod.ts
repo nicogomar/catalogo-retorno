1160fdbe82aaebdb7824a2087de66593bae4e3eb
Nicolas Gonzalez Martinez <nicolas.gonzalez@correo.ucu.edu.uy>
2025-10-31 03:50:48 -0300

    link front y back

--- App/src/environments/environment.prod.ts
+++ App/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@
 
 export const environment = {
   production: true,
-  apiUrl: "https://catalogo-retorno-backend-rioxg2hse.vercel.app/api", // ⬅️ Reemplaza con tu URL de Vercel después del deploy
+  apiUrl: "https://catalogo-retorno-backend.vercel.app/api", // ⬅️ Reemplaza con tu URL de Vercel después del deploy
   // apiUrl: "https://poised-reeva-nicolasgm-184b6c82.koyeb.app/api", // URL anterior de Koyeb (backup)
 };
